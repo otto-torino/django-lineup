30f9d9d401b1ecdfd4a67be1182577e85aa5a2e8
abidibo <abidibo@gmail.com>
2020-12-11 17:09:45 +0100

    removes migrations dependency

--- lineup/migrations/0001_initial.py
+++ lineup/migrations/0001_initial.py
@@ -9,10 +9,6 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
-    ]
-
     operations = [
         migrations.CreateModel(
             name='MenuItem',
