7d9889690a2e0b04c5c900361bf1b7c52df99486
abidibo <abidibo@gmail.com>
2024-06-18 14:50:23 +0200

    docs: fix readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-lineup'
-copyright = u'2020, Otto SRL'
+copyright = u'2020-2024, Otto SRL'
 author = u'abidibo'
 
 # The version info for the project you're documenting, acts as replacement for
